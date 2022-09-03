886706b970eb4de6761f83f4ec9199f738cbe6db
volzoe <996925288@qq.com>
2022-09-03 15:31:09 +0800

    first commit

---
+++ PersonalizedLearning-ejb/build/generated-sources/ap-source-output/entities/QuestionstudentcosttimePK_.java
@@ -0,0 +1,14 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-23T18:35:12")
+@StaticMetamodel(QuestionstudentcosttimePK.class)
+public class QuestionstudentcosttimePK_ { 
+
+    public static volatile SingularAttribute<QuestionstudentcosttimePK, Integer> studentid;
+    public static volatile SingularAttribute<QuestionstudentcosttimePK, Integer> questionid;
+
+}
---
+++ PersonalizedLearning-ejb/src/java/sessionBeans/StudentFacadeLocal.java
@@ -0,0 +1,35 @@
+package sessionBeans;
+
+import entities.School;
+import entities.Student;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author hgs
+ */
+@Local
+public interface StudentFacadeLocal {
+
+    void create(Student student);
+
+    void edit(Student student);
+
+    void remove(Student student);
+
+    Student find(Object id);
+
+    List<Student> findAll();
+
+    List<Student> findRange(int[] range);
+
+    int count();
+
+    List getQueryResultList(String sql);
+    int executUpdate(String sql);
+
+    List<Student> getStudent4School(School school);
+    
+    Student findByName(String studentIdInSchool);
+}
